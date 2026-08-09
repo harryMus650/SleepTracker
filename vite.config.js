6567bd5bebc9d0388e3e55e3fe76099e6db2c8df
harryMus650 <harrycooper650@gmail.com>
2026-08-09 19:08:41 +1000

    Clean up duplicate root files, project lives in sleeplab/

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  server: { host: true },   // host:true so you can open it on your phone over wifi
-});
